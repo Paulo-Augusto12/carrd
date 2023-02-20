d338c9266d8a5f168f9d67a3e423a85b3b81403a
Paulo Augusto <paulo-augusto12@outlook.com>
2023-02-20 20:33:37 -0300

    feat: iniciado desenvolvimento da home page

--- carrd/src/pages/home_page/index.tsx
+++ carrd/src/pages/home_page/index.tsx
@@ -1,14 +1,14 @@
 import { Box, Container, Typography } from "@mui/material";
+import { Main } from "./style";
 
-export function HomePage () {
-
-    return (
-        <Container>
-            <Box>
-                <Typography variant='h5'>
-                    Olá mundo
-                </Typography>
-            </Box>
-        </Container>
-    )
+export function HomePage() {
+  return (
+    <Main>
+      <Box>
+        <Typography variant="h5" sx={{ color: "#9AC6C5" }}>
+          Olá mundo
+        </Typography>
+      </Box>
+    </Main>
+  );
 }
---
+++ carrd/src/pages/home_page/style.ts
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+import styled from "styled-components";
+
+export const Main = styled.div`
+  height: 100vh;
+  background: rgb(91, 42, 134);
+  background: linear-gradient(
+    0deg,
+    rgba(91, 42, 134, 1) 0%,
+    rgba(119, 133, 172, 1) 35%,
+    rgba(0, 212, 255, 1) 100%
+  );
+`;
